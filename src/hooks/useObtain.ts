9a5f96122d0dacb545e1bdb52f7f652b1c67de3f
Gate Is Bug <63226802+gateisbug@users.noreply.github.com>
2023-09-19 00:14:06 +0900

    version up (#13)

--- src/hooks/useObtain.ts
+++ src/hooks/useObtain.ts
@@ -2,14 +2,16 @@ const event_kor = [
 	"상자깡", "메인", "장비개발", "군부연구실", "코어샵", "건조", "서브미션", "해역구조", "통상파괴",
 	"빛과 그림자의 아이리스", "허상의 탑", "동절의 북해", "부흥의 찬송가", "결상점작전", "하늘 아래의 찬송가", "디바인 트레지 코미디",
 	"비추는 나선의 경해", "짙은 자줏빛의 안개", "깊게 울리는 메아리", "맑고 푸른 바다", "거듭되는 평행세계", "아이리스의 천사",
-	"빛나는 정원의 맹세", "영원한 밤의 환광", "작은 약속", "거울에 비친 이색", "접해몽화", "잿빛 폐허", "극지 폭풍"
+	"빛나는 정원의 맹세", "영원한 밤의 환광", "작은 약속", "거울에 비친 이색", "접해몽화", "잿빛 폐허", "극지 폭풍",
+	"어리석은 자의 천칭"
 ]
 
 const event = [
 	"Boxes", "Main", "Gear Lab", "Research", "Core Shop", "Build", "Mission", "Threat", "Daily",
 	"Iris of Light and Dark", "Virtual Tower", "Nothern Overture", "Daedalian Hymn", "Operation Convergence", "Sky Bound Oratorio", "Empyreal Tregicomedy",
 	"Mirror Involution", "Violet Tempest Blooming Lycoris", "Abyssal Refrain", "Upon the Shimmering Blue", "Parallel Superimposition", "Angel of the Iris",
-	"Pledge of the Radiant Court", "Aurora Noctis", "One Small Promise", "Divergent Chessboard", "Dreamwalkers Butterfly", "Revelations of Dust", "Frostfall"
+	"Pledge of the Radiant Court", "Aurora Noctis", "One Small Promise", "Divergent Chessboard", "Dreamwalkers Butterfly", "Revelations of Dust", "Frostfall",
+	"The Fools Scales Event Banner"
 ]
 
 function useObtain(seperator="\t") {
