adf183e3da8161959ad225f9ac23ad439f0610fd
default <default>
2024-02-02 14:58:44 +0900

    add tabs

--- view/src/components/Tabs/Tabs.tsx
+++ view/src/components/Tabs/Tabs.tsx
@@ -1,7 +1,42 @@
+import React from 'react';
 import styled from 'styled-components';
+
+const TabContainer = styled.div`
+  display: block;
+  position: relative;
+`;
+
+const TabBar = styled.div`
+  position: absolute;
+  width: 0;
+  height: 0.125rem;
+  background-color: ${(props) => props.theme.primary};
+`;
+TabBar.defaultProps = {
+  theme: {
+    primary: 'var(--primary, #ffb74d)',
+  },
+};
+
+const TabBox = styled.div``;
 
 const Tab = styled.div``;
 
-const Tabs = styled.div``;
+interface TabProps {
+  item: string[];
+}
 
-const TabBar = styled.div``;
+const Tabs = ({ item }: TabProps) => {
+  return (
+    <TabContainer>
+      <TabBox>
+        {item.map((v, i) => (
+          <Tab key={`${v}_${i}`}>{v}</Tab>
+        ))}
+      </TabBox>
+      <TabBar />
+    </TabContainer>
+  );
+};
+
+export default React.memo(Tabs);
