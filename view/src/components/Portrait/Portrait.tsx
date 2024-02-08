fa2e34119309c52d2a4c7711de544a4ebab9c51f
default <default>
2024-02-08 17:00:14 +0900

    portrait 컴포넌트 추가

--- view/src/components/Portrait/Portrait.tsx
+++ view/src/components/Portrait/Portrait.tsx
@@ -1,7 +1,68 @@
+import React from 'react';
 import styled from 'styled-components';
 
-interface LazyImageProps {
+const LazyImage = styled.img`
+  width: 3rem;
+  height: 3rem;
+`;
 
+const Frame = styled.div`
+  border-radius: 0.25rem;
+  box-sizing: border-box;
+  padding: 0.375rem;
+  border: 0.125rem solid #7e5475;
+
+  &[data-stroke='default'] {
+    border-color: #7e5475; // #666666
+  }
+  &[data-stroke='red'] {
+    border-color: #e4002b; // #d61314
+  }
+  &[data-stroke='yellow'] {
+    border-color: #f3e500; // #f3f521
+  }
+  &[data-stroke='blue'] {
+    border-color: #0047bb; // #0801d9
+  }
+  &[data-stroke='violet'] {
+    border-color: #ea27c2; // #db16db
+  }
+
+  &[data-tier='N'] {
+    background-color: #ddd;
+  }
+  &[data-tier='R'] {
+    background-color: #9fe8ff;
+  }
+  &[data-tier='SR'] {
+    background-color: #c4adff;
+  }
+  &[data-tier='SSR'] {
+    background-color: #ee9;
+  }
+  &[data-tier='UR'] {
+    background: linear-gradient(120deg, #fbffca, #baffbf, #a7efff, #ffabff);
+  }
+`;
+
+interface PortraitProps {
+  src?: string;
+  placeholder?: string;
+  stroke?: 'default' | 'red' | 'yellow' | 'blue' | 'violet';
+  tier?: Tier;
 }
-const LazyImage = styled.img``;
 
+const Portrait = ({
+  placeholder,
+  src,
+  tier = 'R',
+  stroke = 'default',
+}: PortraitProps) => {
+  return (
+    <Frame data-tier={tier} data-stroke={stroke}>
+      <LazyImage src={placeholder} data-src={src} loading='lazy' />
+    </Frame>
+  );
+};
+
+export default React.memo(Portrait);
--- view/src/components/index.ts
+++ view/src/components/index.ts
@@ -2,6 +2,7 @@ export { default as Dropdown } from './Dropdown';
 export { default as Link } from './Link';
 export { default as Loading } from './Loading';
 export { default as Logo } from './Logo';
+export { default as Portrait } from './Portrait';
 export { default as Search } from './Search';
 export { default as Tabs } from './Tabs';
 export { default as Table } from './Table';
