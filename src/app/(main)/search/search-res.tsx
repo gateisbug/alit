66ab71a43bf9ca8440253c6e2767b8926c7f3145
Dawson <94957353+Dawson-Park@users.noreply.github.com>
2024-05-14 15:11:41 +0900

    TODO 작성

--- src/app/(main)/search/search-res.tsx
+++ src/app/(main)/search/search-res.tsx
@@ -4,6 +4,7 @@ interface ResultProps {
   value: string
 }
 
+// TODO: Global State에서 데이터를 포함하기 시작하면 이곳에서 탐색할 수 있도록 변경할 것
 export default function SearchRes({ value }: ResultProps) {
   return (
     <SearchResult>
