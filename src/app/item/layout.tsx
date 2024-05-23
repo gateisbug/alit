e7ba72a0a39fe24859e48d15998a457ae624afc3
Dawson <94957353+Dawson-Park@users.noreply.github.com>
2024-05-23 14:16:19 +0900

    layout render props 취소

--- src/app/item/layout.tsx
+++ src/app/item/layout.tsx
@@ -3,7 +3,6 @@ import { ReactNode, Suspense } from 'react'
 import { CircularProgress } from '@ui'
 
 export default function ItemLayout({ children }: { children: ReactNode }) {
-  // return <main className='header-adder'>{children}</main>
   return (
     <main className='header-adder'>
       <div className='page item index'>
