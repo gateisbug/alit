d06f16e5d5702d7a6e8b3c137fc3d54ae8488ddb
gateisbug <luftwaffe04@naver.com>
2024-06-21 15:33:54 +0900

    next init

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// import { PHASE_DEVELOPMENT_SERVER } from 'next/constants'
+
+/** @type {{output: string, trailingSlash: boolean, images: {formats: (string)[]}, compress: boolean, reactStrictMode: boolean, compiler: {styledComponents: {ssr: boolean}}}} */
+const nextConfig = {
+  reactStrictMode: true,
+  trailingSlash: true,
+  images: {
+    formats: ['image/avif', 'image/webp', 'image/jpg'],
+  },
+  compiler: {
+    styledComponents: {
+      ssr: true,
+    },
+  },
+  compress: true,
+  output: 'export'
+};
 
 export default nextConfig;
