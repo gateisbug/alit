76f7f9ae5b878400401fd35f0d271b3e9549f962
gateisbug <luftwaffe04@naver.com>
2024-07-12 11:11:33 +0900

    nextjs 전환

--- src/app/(main)/links/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as ArcaLiveLink } from './arcalive'
-export { default as GithubLink } from './github'
-export { default as SearchButton } from './searchButton'
--- src/components/(main)/logo.sc.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-const LogoBox = styled.h1`
-  height: fit-content;
-  line-height: 0;
-  user-select: none;
-`
-
-export default LogoBox
