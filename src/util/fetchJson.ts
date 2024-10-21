dd73d940fd0f4159b796f7ad9de926c8d12d310b
gateisbug <luftwaffe04@naver.com>
2024-10-21 12:48:52 +0900

    원본리소스 24-10-20 추가

--- src/util/fetchJson.ts
+++ src/util/fetchJson.ts
@@ -1,7 +1,4 @@
 export default async function fetchItemData() {
   const response = await fetch('json/all.json')
   return ((await response.json()) as unknown as ItemInterface[]) ?? []
-  // const data = await import('../assets/json/all.json')
-  // console.log(data)
-  // return (data as unknown as ItemInterface[]) ?? []
 }
