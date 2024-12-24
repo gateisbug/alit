8a6a12ad27b06a63e65e9f941409d4dc6fd368c1
gateisbug <luftwaffe04@naver.com>
2024-12-24 17:00:09 +0900

    [2.5.0] eslinting - eslinting

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,11 @@ import { lazy, Suspense } from 'react'
 import { useMediaQuery } from 'react-responsive'
 import { Outlet } from 'react-router-dom'
 
+import GlobalHeader from '@app/@global-header/page.tsx'
+
 const ModalRoot = lazy(() => import('./@modal-root/layout.tsx'))
 const SpeedDial = lazy(() => import('./@speed-dial/layout.tsx'))
 const Fonts = lazy(() => import('./font.tsx'))
-import GlobalHeader from '@app/@global-header/page.tsx'
 
 export default function RootLayout() {
   const isTabletOrMobile = useMediaQuery({ query: '(max-width: 768px)' })
