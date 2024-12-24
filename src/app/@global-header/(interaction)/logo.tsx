5f5c7df48f778b068bd9e9ba78b2eb03626dff0a
gateisbug <luftwaffe04@naver.com>
2024-12-24 14:47:26 +0900

    [2.5.0] 테마 컬러 확인 - logo 컬러 수정 - 라이트테마의 배경색 수정

--- src/app/@global-header/(interaction)/logo.tsx
+++ src/app/@global-header/(interaction)/logo.tsx
@@ -1,14 +1,17 @@
+import { useMediaQuery } from 'react-responsive'
 import { Link } from 'react-router-dom'
 
 import { Logo as LogoBox } from '@components/@global-header/styled.ts'
 
 export default function Logo() {
+  const isDarkTheme = useMediaQuery({ query: '(prefers-color-scheme: dark)' })
+
   // noinspection HtmlUnknownTarget
   return (
     <LogoBox title='메인 페이지로 이동'>
       <Link to='/item' className='flex'>
         <img
-          src='assets/logo.png'
+          src={isDarkTheme ? 'assets/logo-dark.png' : 'assets/logo-light.png'}
           width={36}
           height={36}
           alt='logo'
